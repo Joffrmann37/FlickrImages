1874cf37bec23e23354050f33c97c29143b8addd
Joffrey Mann <joffrey@jfammobility.com>
2024-02-15 17:01:57 -0500

    Initial Commit

---
+++ FlickrImages/FlickrImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImagesApp.swift
+//  FlickrImages
+//
+//  Created by Joffrey Mann on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
